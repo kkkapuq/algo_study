bcdb16ecc2ee6d116739c555e9fef4347685b7e4
gvsteve24 <junghyun30124@gmail.com>
2023-03-18 01:14:10 +0900

    기존_백준_상어중학교_수정

--- Wannabe/Python/백준/Implementation/230315_백준_상어중학교/21609.py
+++ Wannabe/Python/백준/Implementation/230315_백준_상어중학교/21609.py
@@ -1,11 +1,14 @@
+import math
 from collections import deque
-from typing import List, tuple
+from typing import List, Tuple
+from pprint import pprint
 import sys
 
 si = sys.stdin.readline
 
 N, M = map(int, si().strip().split())
 grid = [list(map(int, si().strip().split())) for _ in range(N)]
+visit = [[1]*N for _ in range(N)]
 dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 score = 0
 
@@ -27,7 +30,7 @@ def bfs(x, y, block):
 
     return cnt, (locs[0][0], locs[0][1]), locs
 
-def arrange_group() -> List[tuple]:
+def arrange_group() -> List[Tuple]:
     visit = [[1]*N for _ in range(N)]
     groups = []
     for i in range(N):
@@ -39,13 +42,45 @@ def arrange_group() -> List[tuple]:
                     r, base, locs = bfs(i, j, grid[i][j])
                     groups.append((r, base, locs))
     return groups
-def remove():
+def remove(locs):
     # get cnt**2 as score
-    pass
+    for i, j in locs:
+        grid[i][j] = None
 
 def gravity():
-    pass
+    for i in range(N):
+        j = N-1
+        # from bottom, skip nonzero/minus one, locate first zero as fixed index (k) after which nonzero number will replace
+        while True:
+            while j > 0:
+                if grid[j][i] == 0:
+                    break
+                j -= 1
+            k = j-1
+            while k >= 0:
+                if grid[k][i] == -1:
+                    j = k
+                    break
+                if grid[k][i] != 0 and grid[k][i] != -1:
+                    grid[k][i], grid[j][i] = grid[j][i], grid[k][i]
+                    break
+                k -= 1
+            if j == 0 or k == -1:
+                break
+            j -= 1
 
 def rotate_ccw():
-    pass
+    r = math.ceil(N/2)
+    for i in range(r):
+        for j in range(N//2):
+            temp = grid[i][j]
+            grid[i][j] = grid[j][N-1-i]
+            grid[j][N-1-i] = grid[N-1-i][N-1-j]
+            grid[N-1-i][N-1-j] = grid[N-1-j][i]
+            grid[N-1-j][i] = temp
 
+while True:
+    groups = arrange_group()
+    cnt, base, locs = groups[0]
+    score += cnt**2
+    remove(locs)
