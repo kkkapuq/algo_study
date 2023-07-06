73b1965c3e552f446fe1a80c10cce145e4165dc7
Joon <44130863+kkkapuq@users.noreply.github.com>
2023-07-06 12:28:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 268.36 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -5,16 +5,17 @@
 '''
 def solution(elements):
     answer = set()
+    
+    # 길이가 n일 때, 탐색해야 하는 인덱스는 [현재 인덱스 : 현재 인덱스 + 인덱스 % n] 이다.
+    # 예를들어, 길이가 3인 연속 부분 수열에서 현재 인덱스가 4면, 4, 7, 9 임
+    # if i == n-1: temp += elements[i-n]
     n = len(elements)
     
-    elements = elements + elements[:-1]
-    
-    # 처음 아이디어로 걍 해보자...
-    
     for i in range(n):
-        for j in range(n):
-            answer.add(sum(elements[j:j+i+1]))
-    
-    return len(answer)
+        temp = 0
+        for j in range(i, i+n):
+            index = j
+            temp += elements[j%n]
+            answer.add(temp)
 
-solution([7,9,1,1,4])
+    return len(answer)
