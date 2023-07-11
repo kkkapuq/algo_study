2558838aa97b5b5744ecf57bb62a77b9b52fc13b
Joon <44130863+kkkapuq@users.noreply.github.com>
2023-07-11 19:03:58 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,16 +1,15 @@
 def solution(clothes):
-    # 1. 결괏값과 해시맵 선언
-    result = 1
-    hash_map = dict()
+    answer = 1
     
-    # 2. 의상을 종류별로 구분하기
-    for wear, kind in clothes:
-        # 3. get method로 1씩 증가, 없다면 0 삽입
-        hash_map[kind] = hash_map.get(kind, 0) + 1
-        
-    # 3. 모든 조합 계산하기( 입지 않는 경우 포함 )
-    for kind in hash_map:
-        result *= (hash_map[kind] + 1)
+    dic = {}
     
-    # 4. 아무종류의 옷도 입지 않는 경우를 제외한 값 반환
-    return result - 1
+    for cloth, cType in clothes:
+        if cType not in dic:
+            dic[cType] = [cloth]
+        else:
+            dic[cType].append(cloth)
+    
+    for i in dic.values():
+        answer *= len(i) + 1
+    
+    return answer - 1
