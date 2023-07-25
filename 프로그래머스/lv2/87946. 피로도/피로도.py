d84d039426a8769df8b176098d471ba0ff7b5c24
Joon <44130863+kkkapuq@users.noreply.github.com>
2023-07-25 14:06:50 +0900

    [level 2] Title: 피로도, Time: 0.15 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,20 @@
+from itertools import permutations as pm
+
+def solution(k, dungeons):
+    answer = -1
+    
+    # 순열로 풀면 될듯?
+    for i in list(pm(dungeons, len(dungeons))):
+        # 현재 피로도와 클리어한 던전 수
+        pirodo, clear = k, 0
+        for j in i:
+            # 입장 피로도와 소모 피로도
+            toIn, toUse = j[0], j[1]
+            if toIn <= pirodo:
+                pirodo -= toUse
+                clear += 1
+            else:
+                break
+        answer = max(answer, clear)
+    
+    return answer
