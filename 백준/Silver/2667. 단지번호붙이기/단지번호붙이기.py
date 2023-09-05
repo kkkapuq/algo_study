e054f9f9052b10f96b4cadec2d0fd5f81ae14826
Joon <44130863+kkkapuq@users.noreply.github.com>
2023-09-05 14:19:49 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31348 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,37 +1,33 @@
+'''
+문제 : 단지 번호
+링크 : https://www.acmicpc.net/problem/2667
+소요시간 : 30분
+'''
 n = int(input())
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input())))
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
-
-#단지의 세대 수
-nums = []
-count = 0
-
-def dfs(x, y):
-    if x < 0 or x >= n or y < 0 or y >= n:
-        return False
-    if graph[x][y] == 1:
-        global count
-        count += 1
-        graph[x][y] = 0
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            dfs(nx, ny)
-        return True
-    return False
-
-
+graph = [list(map(int, input())) for _ in range(n)]
+apart = []
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+def dfs(r, c, cnt):
+    graph[r][c] = 0
+    if r < 0 or r >= n or c < 0 or c >= n:
+        return
+    
+    for i in range(4):
+        nx, ny = r + dx[i], c + dy[i]
+        # 범위 밖은 탐색 제외
+        if nx < 0 or nx >= n or ny < 0 or ny >= n:
+            continue
+        if graph[nx][ny] == 1:
+            # 방문한 곳은 0으로 넣어서 방문처리
+            graph[nx][ny] = 0
+            cnt = dfs(nx, ny, cnt+1)
+    return cnt
 for i in range(n):
     for j in range(n):
-        # 모임인 집이 존재한다면
-        if dfs(i, j) == True:
-            nums.append(count)
-            count = 0
-
-nums.sort()
-print(len(nums))
-for i in nums:
+        if graph[i][j] == 1:
+            apart.append(dfs(i, j, 1))
+            
+apart.sort()
+print(len(apart))
+for i in apart:
     print(i)
