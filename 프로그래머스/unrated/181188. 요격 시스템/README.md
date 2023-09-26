89b528f56abee6d0190c494244259c9a742064d2
Joon <44130863+kkkapuq@users.noreply.github.com>
2023-09-26 22:10:26 +0900

    [unrated] Title: 요격 시스템, Time: 0.75 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/README.md
+++ 프로그래머스/unrated/181188. 요격 시스템/README.md
@@ -1,10 +1,10 @@
 # [unrated] 요격 시스템 - 181188 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 73.6 MB, 시간: 0.75 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 문제 설명
 
---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 1;
+        
+        // a, b중 compare가 정순이면 오름차순, 역순이면 내림차순
+        Arrays.sort(targets, (a, b) -> {
+            return a[1]-b[1];
+        });
+        int cur = targets[0][1]-1;
+        for(int[] arr : targets){
+            int s = arr[0];
+            int e = arr[1];
+            
+            if(cur >= s && cur <= e) continue;
+            
+            answer++;
+            cur = e-1;
+        }
+        
+        return answer;
+    }
+}
