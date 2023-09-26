5168cfaa7da50c722e08e62569235f0855126732
Hyungjoon Cho <fhrek@naver.com>
2023-09-26 22:42:04 +0900

    폴더 정리 및 자바 4문제 풀이

---
+++ Eungae/Java/untitled/src/backjoon/Implementation/Silver_1_2002/Solved.java
@@ -0,0 +1,43 @@
+package backjoon.Implementation.Silver_1_2002;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 문제 : 추월
+ * 링크 : https://www.acmicpc.net/problem/2002
+ * 소요시간 :
+ */
+
+public class Solved {
+    static LinkedHashMap<String, Integer> in = new LinkedHashMap<>();
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            String car = br.readLine().toString();
+            in.put(car, i);
+        }
+
+        for (int i = 0; i < N; i++) {
+            String outCar = br.readLine().toString();
+            Iterator<String> it = in.keySet().iterator();
+
+            while (it.hasNext()) {
+                if (in.get(outCar) > in.get(it.next())) {
+                    answer++;
+                    break;
+                }
+            }
+
+            in.remove(outCar);
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
+}
