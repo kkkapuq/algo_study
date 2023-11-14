a5176acf597e453b2049c45e13d563bf52238047
Joon <44130863+kkkapuq@users.noreply.github.com>
2023-11-14 22:59:29 +0900

    [Silver I] Title: 기타 레슨, Time: 344 ms, Memory: 25932 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        // 이분탐색 수행을 위한 start와 end값 설정
+        int s = Arrays.stream(arr).max().orElse(0);
+        int e = Arrays.stream(arr).sum();
+
+        while (s <= e) {
+            // 블루레이별 길이와 수
+            int total = 0;
+            int cnt = 1;
+
+            int mid = (s + e) / 2;
+            for (int time:arr) {
+                if (total + time > mid) {
+                    total = 0;
+                    cnt += 1;
+                }
+                total += time;
+            }
+            if (cnt <= m) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        System.out.println(s);
+    }
+}
