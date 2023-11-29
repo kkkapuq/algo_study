132feb2737dca53ccc13100a5cc19181c5aaa7c7
Joon <44130863+kkkapuq@users.noreply.github.com>
2023-11-29 19:15:54 +0900

    [level 2] Title: 요격 시스템, Time: 1.74 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -5,14 +5,23 @@ class Solution {
         double cur = -1;
         int answer = 0;
 
-        Arrays.sort(targets, (a, b) -> {
-            return a[1] - b[1];
-        });
+        // 2차원 배열을 List<List<Integer>>으로 변환
+        List<List<Integer>> targetList = new ArrayList<>();
+        for (int[] target : targets) {
+            List<Integer> tempList = new ArrayList<>();
+            for (int value : target) {
+                tempList.add(value);
+            }
+            targetList.add(tempList);
+        }
 
-        for(int i = 0; i < targets.length; i++){
-            if(cur < targets[i][0]){
+        // 두 번째 열을 기준으로 오름차순 정렬
+        Collections.sort(targetList, Comparator.comparingInt(list -> list.get(1)));
+
+        for (List<Integer> target : targetList) {
+            if (cur < target.get(0)) {
                 answer++;
-                cur = targets[i][1] - 0.5;
+                cur = target.get(1) - 0.5;
             }
         }
 
