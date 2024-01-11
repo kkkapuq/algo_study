8cf1136591283764c62dd70ab7edf1d37ff006ca
Joon <44130863+kkkapuq@users.noreply.github.com>
2024-01-11 21:54:52 +0900

    [level Lv.1] Title: 바탕화면 정리, Time: 0.12 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int[] answer = {};
+        
+        // S는 파일이 존재하는 선에서 제일 왼쪽+위쪽
+        // E는         ''       제일 아래쪽+오른쪽
+        // N^2로 돌고나면 알 수 있다.
+        
+        // lux, luy, rdx, rdy의 최소,최댓값을 계속 업데이트해주면 된다.
+        // lux, luy는 최소
+        // rdx, rdy는 최대값으로 업데이트해주자.
+        
+        int lux = 51, luy = 51;
+        int rdx = -1, rdy = -1;
+        
+        int n = wallpaper.length;
+        int m = wallpaper[0].length();
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(wallpaper[i].charAt(j) == '#'){
+                    // System.out.println("현재좌표 : " + i + "," + j);
+                    
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+                    rdx = Math.max(rdx, i);
+                    rdy = Math.max(rdy, j);
+                    
+//                     System.out.println("lux : " + lux);
+//                     System.out.println("luy : " + luy);
+//                     System.out.println("rdx : " + rdx);
+//                     System.out.println("rdy : " + rdy);
+                }
+            }
+        }
+        // 커서가 끝나는 오른쪽은 파일을 포함해야 해서 +1 해줘야함.
+        answer = new int[]{lux, luy, rdx+1, rdy+1};
+        
+        return answer;
+    }
+    
+}
